b07bdee037387da8ce61a525bbed15f99e9fdee7
CatalinaJL <catalina.p.jl@gmail.com>
2021-03-05 23:44:01 -0300

    Feat: Date/Hour feature. Structure, and try to useState to make it visible

---
+++ src/Components/DateHour.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+
+export const DateHour = () =>{
+    const [showDateHour, setShowDateHour] = useState(false)
+    return(
+       <div>
+           <input type="date" name="" id=""/>
+           <input type="time" name="" id=""/>
+       </div>
+    )
+};
+
--- src/Components/Items-List.jsx
+++ src/Components/Items-List.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Radio } from '@material-ui/core';
 import triangle from '../Assets/Triangle.jpg';
+
+
+const DateHour = () =>{
+    const [showDateHour, setShowDateHour] = useState(false)
+    return(
+       <div>
+           <input type="date" name="" id=""/>
+           <input type="time" name="" id=""/>
+       </div>
+    )
+};
+
+const HandleChange= () =>{
+    console.log('estan seleccionando')
+} ;
+
 
 export const ItemsList = () =>{
     return(
         <div className="itemsList">
             <div className="containerItem">
-            <Radio name= "Problem" onChange={console.log('me estan selecionando')}></Radio>
+            <Radio name= "Problem" onChange={HandleChange}></Radio>
                 <div className="card">
                     <div className="card-text">
                         <h2>Problem</h2>
@@ -15,10 +31,9 @@ export const ItemsList = () =>{
                     <div className="card-img">
                         <img src={triangle} alt=""/>
                     </div>
-                </div>
-                <div className="date-hour">
-                    
-
+                    <div>
+                        <DateHour/>
+                    </div>
                 </div>
             </div>
           
