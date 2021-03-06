8a5996082bc2240883cf86ebdae4c430bc7ed038
CatalinaJL <catalina.p.jl@gmail.com>
2021-03-06 18:05:31 -0300

    Feat: Add of array with titles of cards in Ideation section

--- src/Components/Items-List.jsx
+++ src/Components/Items-List.jsx
@@ -4,6 +4,7 @@ import triangle from '../Assets/Triangle.jpg';
 import {DateHour} from './DateHour';
 
 
+const dataCard=['Problem', 'Solution', 'Team']
 
 export const ItemsList = () =>{
     const [displayDateHour, setDisplayDateHour]= useState(false);
@@ -11,26 +12,42 @@ export const ItemsList = () =>{
     const HandleChange= () =>{
         console.log('estan seleccionando');
         setDisplayDateHour(true);
+        // if (displayDateHour(false)) {
+        // setDisplayDateHour(true);
+        // } else{
+        //     setDisplayDateHour(false)
+        // }
     } ; 
-       
+
     return(
         <div className="itemsList">
-            <div className="containerItem">
-            <Radio name= "Problem" onChange={HandleChange}></Radio>
-                <div className="card">
-                    <div className="card-text">
-                        <h2>Problem</h2>
-                        <p>view Builder</p>
-                    </div>
-                    <div className="card-img">
-                        <img src={triangle} alt=""/>
-                    </div>
-                    <div>
-                        {displayDateHour && <DateHour/>}
-                        {!displayDateHour && null}
-                    </div>
+                <div className="headerBox">
+                    <h4>Ideation</h4>
+                    <span>Select all in ideation</span>
+                    <p>Due Date (optional)</p>
                 </div>
-            </div>
+                {dataCard.map((data, index)=>{
+                    return(
+                    <div className="containerItem" key={index}>
+                        <Radio name="topic" onChange={HandleChange}></Radio>
+                        <div className="card">
+                            <div className="card-text">
+                                <h4>{data}</h4>
+                                <p>view Builder</p>
+                            </div>
+                            <div className="card-img">
+                                <img src={triangle} alt=""/>
+                            </div>
+                           
+                        </div>
+                        <div>
+                                {displayDateHour && <DateHour/>}
+                                {!displayDateHour && null}
+                        </div>
+                    </div>)
+                })}
+           
+            
           
 
         
